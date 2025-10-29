f21018e7617aa350d752176d5c97ff5718bb1096
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-29 14:30:00 +0800

    Add get and update methods to UserController for user retrieval and modification

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -16,4 +16,17 @@ export class UserController extends BaseController {
         const user = await this.service.create(req.body);
         return this.created(res, user);
     });
+
+    get = this.handler(async (req: Request, res: Response) => {
+        const id = Number(req.params.id);
+        const user = await this.service.getById(id);
+        if (!user) return this.fail(res, "User not found", 404);
+        return this.ok(res, user);
+    });
+
+    update = this.handler(async (req: Request, res: Response) => {
+        const id = Number(req.params.id);
+        const user = await this.service.update(id, req.body);
+        return this.ok(res, user);
+    });
 }
