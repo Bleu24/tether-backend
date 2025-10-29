36a5faaa4b46df9fdb0969f9b6969afb97d7f386
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-29 14:29:56 +0800

    Add ProfilePreferenceController for managing user profile preferences

---
+++ src/controllers/ProfilePreferenceController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { BaseController } from "./BaseController";
+import { ProfilePreferenceService } from "../services/ProfilePreferenceService";
+
+export class ProfilePreferenceController extends BaseController {
+    constructor(private readonly service: ProfilePreferenceService) {
+        super();
+    }
+
+    get = this.handler(async (req: Request, res: Response) => {
+        const userId = Number(req.params.userId);
+        const pref = await this.service.get(userId);
+        return this.ok(res, pref);
+    });
+
+    update = this.handler(async (req: Request, res: Response) => {
+        const userId = Number(req.params.userId);
+        const pref = await this.service.update(userId, req.body);
+        return this.ok(res, pref);
+    });
+}
