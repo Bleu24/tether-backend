b45422bf5746b3f576dd3acd09fd2bebdd8b8a4c
Bleu24 <bryanreamill2403@gmail.com>
2025-10-30 20:39:22 +0800

    Add user ID validation in ProfilePreferenceController

--- src/controllers/ProfilePreferenceController.ts
+++ src/controllers/ProfilePreferenceController.ts
@@ -9,12 +9,18 @@ export class ProfilePreferenceController extends BaseController {
 
     get = this.handler(async (req: Request, res: Response) => {
         const userId = Number(req.params.userId);
+        if (!Number.isFinite(userId) || userId <= 0) {
+            return this.fail(res, "Invalid user id", 400);
+        }
         const pref = await this.service.get(userId);
         return this.ok(res, pref);
     });
 
     update = this.handler(async (req: Request, res: Response) => {
         const userId = Number(req.params.userId);
+        if (!Number.isFinite(userId) || userId <= 0) {
+            return this.fail(res, "Invalid user id", 400);
+        }
         const pref = await this.service.update(userId, req.body);
         return this.ok(res, pref);
     });
